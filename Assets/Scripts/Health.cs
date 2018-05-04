1aa7ae32daf592330ab7534c9f5a91e71866976e
topleftstyll <joshua.brackett@hotmail.ca>
2018-05-04 17:30:14 -0400

    added health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float m_maxHealth = 100.0f;
+	
+	private float m_currHealth;
+
+	void Start() {
+		m_currHealth = m_maxHealth;
+	}
+
+	public void TakeDamage(float damage) {
+		m_currHealth -= damage;
+		if(m_currHealth <= 0) {
+			m_currHealth = 0;
+			Dead();
+		}
+	}
+
+	public void Heal(float healthAmt) {
+		m_currHealth += healthAmt;
+		if(m_currHealth >= m_maxHealth) {
+			m_currHealth = m_maxHealth;
+		}
+	}
+
+	void Dead() {
+		Debug.Log("dead");
+	}
+}
