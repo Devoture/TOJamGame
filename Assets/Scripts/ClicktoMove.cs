de66e0cdb0fa38f3deb50d9cdf88233b448084ef
topleftstyll <joshua.brackett@hotmail.ca>
2018-05-05 01:49:54 -0400

    maybe fixed movement

--- Assets/Scripts/ClicktoMove.cs
+++ Assets/Scripts/ClicktoMove.cs
@@ -3,21 +3,26 @@ using UnityEngine.AI;
 
 public class ClicktoMove : MonoBehaviour {
 	NavMeshAgent agent;
+	bool hasMoved = false;
+	private Animator m_animator;
 	
 	void Start() {
 		agent = GetComponent<NavMeshAgent>();
+		m_animator = GetComponent<Animator>();
 	}
 	
 	void Update() {
 		if (Input.GetMouseButton(0)) {
 			RaycastHit hit;
-			
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
 				agent.destination = hit.point;
-				
+				m_animator.SetBool("isMoving", true);
+				hasMoved = true;	
 			}
 		}
+		if(agent.velocity == Vector3.zero && hasMoved) {
+			Debug.Log("arrived");
+			m_animator.SetBool("isMoving", false);
+		}
 	}
-
-	
 }
