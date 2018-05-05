bb20b9261a559beea9e7814caed257d0372a3480
topleftstyll <joshua.brackett@hotmail.ca>
2018-05-04 22:46:57 -0400

    blackhole pull and push are working

--- Assets/Scripts/CharacterAbilities.cs
+++ Assets/Scripts/CharacterAbilities.cs
@@ -8,7 +8,11 @@ public class CharacterAbilities : MonoBehaviour {
 	public Transform m_fireballSpawnPoint;
 	public float m_fireballSpeed = 5.0f;
 	public float m_heroicLeepSpeed = 5.0f;
-	public Collider m_blackholdCollider;
+	public float m_pullRadius = 10.0f;
+	public float m_pullForce = 200.0f;
+	public float m_pushForce = 100.0f;
+	public bool m_isBlackhole = false;
+
 	private bool m_heroicLeeping = false;
 
 	void Start() {
@@ -33,6 +37,27 @@ public class CharacterAbilities : MonoBehaviour {
 		}
 	}
 
+	public void FixedUpdate() {
+		if(m_isBlackhole) {
+			foreach (Collider collider in Physics.OverlapSphere(transform.position, m_pullRadius)) {
+				if(collider.gameObject.tag == "Enemy") {
+					Vector3 forceDirection = transform.position - collider.transform.position;
+					collider.GetComponent<Rigidbody>().AddForce(forceDirection * m_pullForce * Time.fixedDeltaTime);
+				}
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.F)) {
+			m_isBlackhole = false;
+			foreach (Collider collider in Physics.OverlapSphere(transform.position, m_pullRadius)) {
+				if(collider.gameObject.tag == "Enemy") {
+					Vector3 forceDirection = transform.position + collider.transform.position;
+					collider.GetComponent<Rigidbody>().AddForce(forceDirection * m_pushForce * Time.fixedDeltaTime);
+				}
+			}
+		}
+	}
+
 	void FireBall() {
 		GameObject fireball = Instantiate(m_fireballPrefab, m_fireballSpawnPoint.position, Quaternion.identity);
 		fireball.transform.LookAt(GetMousePos());
@@ -48,7 +73,7 @@ public class CharacterAbilities : MonoBehaviour {
 	}
 
 	void Blackhole() {
-		m_blackholdCollider.enabled = true;
+		m_isBlackhole = true;
 	}
 
 	Vector3 GetMousePos() {
