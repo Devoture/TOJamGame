82dcfa33c99e7ee83c62d7b390898c795e1287cb
MaximumCarnage <dylansbruton@gmail.com>
2018-05-04 20:50:09 -0400

    working on fixing NavMesh

--- Assets/Scripts/ClicktoMove.cs
+++ Assets/Scripts/ClicktoMove.cs
@@ -9,12 +9,15 @@ public class ClicktoMove : MonoBehaviour {
 	}
 	
 	void Update() {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButton(0)) {
 			RaycastHit hit;
 			
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
 				agent.destination = hit.point;
+				
 			}
 		}
 	}
+
+	
 }
