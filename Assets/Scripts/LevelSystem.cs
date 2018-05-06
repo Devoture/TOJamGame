fb0a472c49fe97571ff16ef463f3a90e7e836bbc
MaximumCarnage <dylansbruton@gmail.com>
2018-05-06 13:21:46 -0400

    LevelSystem complete, HudSetUp

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSystem : MonoBehaviour {
+	public float m_maxXP = 1000.0f;
+	public Image m_XPFill;	
+	public Text m_leveldisplay;
+	private float m_currentXP;
+	private float m_totalLevel;
+	// Use this for initialization
+	void Start () {
+		m_currentXP = m_maxXP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)){
+			GainXp(100);
+		}
+	}
+
+	public void GainXp(float xpPoints) {
+			m_currentXP += xpPoints;
+
+			if(m_currentXP >= m_maxXP) {
+				LevelUp();
+			}
+
+			UpdateHUD();
+			
+
+		
+	}
+
+	public void LevelUp(){
+		m_totalLevel++;
+		m_currentXP = 0;
+		UpdateHUD();
+	}
+	public void UpdateHUD (){
+		m_leveldisplay.text ="Level: " +  m_totalLevel;
+		m_XPFill.fillAmount = (float)m_currentXP / (float)m_maxXP;
+	}
+}
---
+++ Assets/Scripts/ManaScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaScript : MonoBehaviour {
+
+public float m_maxMana = 100.0f;
+
+	public bool m_isPlayer;
+
+	public Image m_manaFill;
+	
+	private float m_currentMana;
+
+	void Start() {
+		m_currentMana = m_maxMana;
+	}
+
+	public void UseMana(float manaCost) {
+			m_currentMana -= manaCost;
+
+			if(m_currentMana <= 0) {
+
+			}
+			if(m_isPlayer){
+				UpdateHUD();
+			}
+
+		
+	}
+
+	public void RegainMana(float healthAmt) {
+		m_currentMana += healthAmt;
+		if(m_currentMana >= m_maxMana) {
+			m_currentMana = m_maxMana;
+		}
+		if(m_isPlayer){
+			UpdateHUD();
+		}
+	}
+
+	void Dead() {
+	}
+
+	void UpdateHUD() {
+		m_manaFill.fillAmount = (float)m_currentMana / (float)m_maxMana;
+	}
+}
