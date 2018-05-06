e906df90ac06b3b6aa253492c83b01c7d6c7e652
Tom Sauer <tom@exluto.com>
2018-05-06 18:02:56 -0400

    starting animalAI script, added main level to testing scene

---
+++ Assets/Scripts/AnimalAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalAI : MonoBehaviour {
+	public NavMeshAgent agent;
+
+	public enum State {
+		HERDING,
+		RUNNING
+	}
+
+	public State state;
+	public bool alive;
+	public GameObject[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
