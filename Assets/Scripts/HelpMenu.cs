4583d971a5cd7b020ddc672977fe3824832c5e12
MaximumCarnage <dylansbruton@gmail.com>
2018-05-06 05:19:35 -0400

    finalized menus with help pop up

---
+++ Assets/Scripts/HelpMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpMenu : MonoBehaviour {
+
+	public GameObject m_helpCanvas;
+	public GameObject m_menuCanvas;
+
+	void Start() {
+		CloseHelp();
+	}
+
+	public void Help() {
+		m_helpCanvas.SetActive(true);
+		m_menuCanvas.SetActive(false);
+
+	}
+
+	public void CloseHelp() {
+		m_helpCanvas.SetActive(false);
+		m_menuCanvas.SetActive(true);
+	}
+}
